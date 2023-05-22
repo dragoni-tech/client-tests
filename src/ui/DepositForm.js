29d4ab95828b0f12277316f51684079bd4377a0d
Tobias Downer <toby@mckoi.com>
2023-05-22 18:52:23 +0100

    Support for stored cards.

--- src/ui/DepositForm.js
+++ src/ui/DepositForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Header, Icon, Button, Form, Input, Select, Accordion } from 'semantic-ui-react';
+import { Header, Icon, Button, Form, Input, Select, List, Segment, Accordion } from 'semantic-ui-react';
 
 function custDetails(props, idv) {
     if (props.customer_details !== undefined) {
@@ -30,13 +30,15 @@ const DepositForm = (props) => {
     const [ billing_postcode, setBillingPostcode ] =
                     useState(custDetails(props, "billing_postcode"));
 
-    const [currency, setCurrency] = useState('EFM');
+    const [currency, setCurrency] = useState('GBP');
     const [amount, setAmount] = useState('');
     const [payment_method, setPaymentMethod] = useState('VISA');
     const [pan, setPan] = useState('');
     const [expiry, setExpiry] = useState('');
     const [cvv, setCvv] = useState('');
     const [holder_name, setHolderName] = useState('');
+
+    const [selected_card_id, setSelectedCardId] = useState('');
 
     const [billing_open, setBillingOpen] = useState(false);
 
@@ -60,16 +62,102 @@ const DepositForm = (props) => {
             billing_address1, billing_address2, billing_address3,
             billing_city, billing_county, billing_postcode,
 
-            currency, amount, payment_method, pan, expiry, cvv, holder_name
+            currency, amount,
+            selected_card_id, payment_method, pan, expiry, cvv, holder_name
 
         });
     };
+
+    const handleStoredCardSelection = (card_id) => {
+        let to_set = '';
+        if (selected_card_id !== card_id) {
+            to_set = card_id;
+        }
+        console.log(to_set);
+        setSelectedCardId(to_set);
+    };
+
 
     const handleBillingToggle = () => {
         setBillingOpen(!billing_open);
     };
 
+    // The current cards stored for the customer,
+    const customer_stored_cards = props.customer_stored_cards;
+
     const disabled = props.disabled;
+
+    // The UI to present for credit card entry (changes depending on if a card
+    // is selected or not),
+    let card_entry_content;
+    if (selected_card_id === '') {
+        card_entry_content = (
+            <div>
+                <Form.Field width={4}
+                    disabled={disabled}
+                    control={Select}
+                    label="Payment Method"
+                    options={paymentOptions}
+                    placeholder="Payment Method"
+                    value={payment_method}
+                    onChange={(e, { value }) => setPaymentMethod(value)}
+                />
+                <Form.Field
+                    disabled={disabled}
+                    control={Input}
+                    label="Card Number"
+                    placeholder="Card Number"
+                    value={pan}
+                    onChange={(e) => setPan(e.target.value)}
+                />
+                <Form.Group widths="equal">
+                    <Form.Field
+                        disabled={disabled}
+                        control={Input}
+                        label="Expiry Date"
+                        placeholder="MM/YY"
+                        value={expiry}
+                        onChange={(e) => setExpiry(e.target.value)}
+                    />
+                    <Form.Field
+                        disabled={disabled}
+                        control={Input}
+                        label="CVV"
+                        placeholder="CVV"
+                        value={cvv}
+                        onChange={(e) => setCvv(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Field
+                    disabled={disabled}
+                    control={Input}
+                    label="Name as appears on card"
+                    placeholder="Card holders name"
+                    value={holder_name}
+                    onChange={(e) => setHolderName(e.target.value)}
+                />
+            </div>
+        );
+    }
+
+    // Otherwise user has selected a card so only requires CVV to be passed,
+    else {
+        card_entry_content = (
+            <div>
+                <Form.Group widths="equal">
+                    <Form.Field
+                        disabled={disabled}
+                        control={Input}
+                        label="Confirm CVV"
+                        placeholder="CVV"
+                        value={cvv}
+                        onChange={(e) => setCvv(e.target.value)}
+                    />
+                </Form.Group>
+            </div>
+        );
+    }
+
 
     return (
         <Form onSubmit={handleSubmit}>
@@ -165,50 +253,28 @@ const DepositForm = (props) => {
                     onChange={(e) => setAmount(e.target.value)}
                 />
             </Form.Group>
-            <Form.Field width={4}
-                disabled={disabled}
-                control={Select}
-                label="Payment Method"
-                options={paymentOptions}
-                placeholder="Payment Method"
-                value={payment_method}
-                onChange={(e, { value }) => setPaymentMethod(value)}
-            />
-            <Form.Field
-                disabled={disabled}
-                control={Input}
-                label="Card Number"
-                placeholder="Card Number"
-                value={pan}
-                onChange={(e) => setPan(e.target.value)}
-            />
-            <Form.Group widths="equal">
-                <Form.Field
-                    disabled={disabled}
-                    control={Input}
-                    label="Expiry Date"
-                    placeholder="MM/YY"
-                    value={expiry}
-                    onChange={(e) => setExpiry(e.target.value)}
-                />
-                <Form.Field
-                    disabled={disabled}
-                    control={Input}
-                    label="CVV"
-                    placeholder="CVV"
-                    value={cvv}
-                    onChange={(e) => setCvv(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Field
-                disabled={disabled}
-                control={Input}
-                label="Name as appears on card"
-                placeholder="Card holders name"
-                value={holder_name}
-                onChange={(e) => setHolderName(e.target.value)}
-            />
+
+            <Segment>
+                <Header as="h4">Stored Cards</Header>
+                <List divided selection verticalAlign="middle">
+                    { customer_stored_cards.map((card) => (
+                        <List.Item active={card.cc_cardstore_id === selected_card_id}
+                                   key={card.cc_cardstore_id}
+                                   onClick={ () => handleStoredCardSelection( card.cc_cardstore_id ) }>
+                            <List.Content>{ card.card_holder_name }</List.Content>
+                            <List.Content>{ card.card_type }: { card.card_pan_label } Exp: { card.card_expiration }</List.Content>
+                        </List.Item>
+                      ))
+                    }
+                </List>
+            </Segment>
+
+            { card_entry_content }
+
+            <br/>
+
             <Button type="submit" disabled={disabled} >Start Deposit</Button>
+
         </Form>
     );
 };
--- src/ui/DepositTest.js
+++ src/ui/DepositTest.js
@@ -25,6 +25,7 @@ const DepositTest = (props) => {
     const [step, setStep] = useState('uninit');
     const [transaction_id, setTransactionId] = useState('');
     const [payment_customer_details, setPaymentCustomerDetails] = useState({});
+    const [customer_stored_cards, setCustomerStoredCards] = useState([]);
 
     const [operation_error, setOperationError] = useState({});
 
@@ -87,7 +88,6 @@ const DepositTest = (props) => {
             const {
                 transaction_id,
                 payment_charge_details,
-                stored_cards,         // All stored cards for this user,
                 permitted_currencies, // Array of permitted currency codes,
             } = init_pay_charge_response;
 
@@ -98,6 +98,9 @@ const DepositTest = (props) => {
             // Customer details from the payment charge details,
             const customer_details = payment_charge_details.customer_details;
 
+            // All stored cards for this user,
+            const stored_cards = payment_charge_details.stored_cards;
+
             // If the deposit display is 'REGULAR_CC_FIELDS' then we accept
             // the pan/cvv directly from the user,
             if (payment_charge_details.display === 'REGULAR_CC_FIELDS') {
@@ -105,11 +108,10 @@ const DepositTest = (props) => {
                 // Set up UI with the customer details,
                 setPaymentCustomerDetails(customer_details);
 
-                // PENDING: Set up UI for the stored cards and restrict to
-                //   permitted currencies,
+                // Set up UI for the stored cards,
+                setCustomerStoredCards(stored_cards);
 
-
-
+                // Set step to display regular CC fields (server 2 server);
                 setStep('init-regular-cc-fields');
 
             }
@@ -150,7 +152,8 @@ const DepositTest = (props) => {
             billing_city, billing_county, billing_postcode,
 
             currency, amount,
-            payment_method, pan, expiry, cvv, holder_name
+            selected_card_id, payment_method, pan, expiry, cvv, holder_name
+
         } = details;
 
         await setIsProcessing(true);
@@ -162,13 +165,21 @@ const DepositTest = (props) => {
         const context = props.ecl_context;
 
         // Card info,
-        const cc_info = {
-            pan,
-            expiration: expiry,
-            securitycode: cvv,
-            holdername: holder_name
-        };
-        const save_in_card_store = false;
+        const cc_info = {};
+
+        // If we are using a selected card number,
+        if (selected_card_id !== undefined && selected_card_id !== '') {
+            cc_info.storedcreditcardid = selected_card_id;
+            cc_info.securitycode = cvv;
+        }
+        else {
+            cc_info.pan = pan;
+            cc_info.expiration = expiry;
+            cc_info.securitycode = cvv;
+            cc_info.holdername = holder_name;
+        }
+
+        const save_in_card_store = true;
 
         // PaySafe doesn't allow redirects to 'localhost' so we use a REDIRECT
         // middleware component for testing.
@@ -237,6 +248,7 @@ const DepositTest = (props) => {
                     <DepositForm
                         onDepositAction = { handleRegularCCFieldsDepositAction }
                         customer_details = { payment_customer_details }
+                        customer_stored_cards = { customer_stored_cards }
                         disabled = { is_processing }
                     />
                 </Grid.Column>
